12ced5802d1a84d663980125d2d02fcefea29605
LordLizard Notebook <WarriorXKZ@gmail.com>
2024-12-30 12:41:04 +0300

    add + and - operators

--- utils/time_util.cpp
+++ utils/time_util.cpp
@@ -5,12 +5,18 @@ TimeUtil::TimeUtil(const std::string& time) {
     std::string hour_str = time.substr(0, 2);
     std::string minute_str = time.substr(3, 2);
 
-    if (std::stoi(hour_str) > 24 || std::stoi(minute_str) > 60) {
+    if (std::stoi(hour_str) > 23 || std::stoi(minute_str) > 59) {
         throw std::invalid_argument("Invalid time format");
     }
 
     hour = std::stoi(hour_str);
     minute = std::stoi(minute_str);
+}
+
+TimeUtil::TimeUtil(int hour, int minute) : hour(hour), minute(minute) {
+    if (hour < 0 || hour > 23 || minute < 0 || minute > 59) {
+        throw std::out_of_range("Time values out of range.");
+    }
 }
 
 bool TimeUtil::operator<(const TimeUtil& other) const {
@@ -22,3 +28,36 @@ bool TimeUtil::operator<(const TimeUtil& other) const {
         return false;
     }
 }
+
+TimeUtil TimeUtil::operator+(const TimeUtil& other) const {
+    int newHour = hour + other.hour;
+    int newMinute = minute + other.minute;
+
+    if (newMinute >= 60) {
+        newMinute -= 60;
+        newHour += 1;
+    }
+
+    // if (newHour >= 24) {
+    //     newHour -= 24;
+    // }
+
+    return TimeUtil(newHour, newMinute);
+}
+
+TimeUtil TimeUtil::operator-(const TimeUtil& other) const {
+    int newHour = hour - other.hour;
+    int newMinute = minute - other.minute;
+
+    if (newMinute < 0) {
+        newMinute += 60;
+        newHour -= 1;
+    }
+
+    // if (newHour < 0) {
+    //     newHour += 24;
+    // }
+
+    return TimeUtil(newHour, newMinute);
+}
+
--- utils/time_util.h
+++ utils/time_util.h
@@ -10,7 +10,12 @@ private:
 
 public:
     TimeUtil(const std::string& time);
+    TimeUtil(int hour, int minute);
+
     bool operator<(const TimeUtil& other) const;
+
+    TimeUtil operator+(const TimeUtil& other) const;
+    TimeUtil operator-(const TimeUtil& other) const;
 
     bool operator>(const TimeUtil& other) const { return other < *this; }
     bool operator<=(const TimeUtil& other) const { return !(other < *this); }
