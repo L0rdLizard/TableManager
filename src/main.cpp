241ce550cf756d41444322c245314d34ff4079bd
LordLizard Notebook <WarriorXKZ@gmail.com>
2025-01-03 03:15:14 +0300

    test main

--- src/main.cpp
+++ src/main.cpp
@@ -1,5 +1,11 @@
 #include "../include/EventManager.h"
+#include <iostream>
 
 int main(int argc, char* argv[]) {
-    
+    std::string path = "../input/day1.txt";
+    EventManager eventManager(path);
+    eventManager.loadEvents();
+    eventManager.processEvents();
+    eventManager.printEventLog();
+    return 0;
 }
