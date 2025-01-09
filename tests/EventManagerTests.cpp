30f76c3ed124af6f237cfcd7166eb0ca57092aed
L0rdLizard <WarriorXKZ@gmail.com>
2025-01-09 19:03:39 +0300

    Feat: divide test to TestProcessEventsError and TestProcessEvents

--- tests/EventManagerTests.cpp
+++ tests/EventManagerTests.cpp
@@ -12,11 +12,6 @@ TEST(EventManagerTest, TestLoadEvents) {
     MockTableManager* mockTableManager = new MockTableManager(tableCount, hourlyRate);
     MockClientManager* mockClientManager = new MockClientManager();
 
-    // EXPECT_CALL(*mockTableManager, isAnyFreeTable())
-    //     .WillOnce(testing::Return(true));
-
-    // EXPECT_CALL(*mockClientManager, registerClient("client1"));
-
     EventManager eventManager(
         "test_file.txt",
         mockTableManager,
@@ -36,26 +31,6 @@ TEST(EventManagerTest, TestLoadEvents) {
         "12:33 4 client1"
     };
 
-    // std::vector<std::string> eventLines = {
-    //     "3",
-    //     "09:00 19:00",
-    //     "10",
-    //     "08:48 1 client1",
-    //     "09:41 1 client1",
-    //     "09:48 1 client2",
-    //     "09:52 3 client1",
-    //     "09:54 2 client1 1",
-    //     "10:25 2 client2 2",
-    //     "10:58 1 client3",
-    //     "10:59 2 client3 3",
-    //     "11:30 1 client4",
-    //     "11:35 2 client4 2",
-    //     "11:45 3 client4",
-    //     "12:33 4 client1",
-    //     "12:43 4 client2",
-    //     "15:52 4 client4"
-    // };
-
     bool result = eventManager.loadEvents(eventLines);
     EXPECT_TRUE( result );
 }
@@ -69,6 +44,26 @@ TEST (EventManagerTest, TestProcessEvents) {
     MockTableManager* mockTableManager = new MockTableManager(tableCount, hourlyRate);
     MockClientManager* mockClientManager = new MockClientManager();
 
+    EXPECT_CALL(*mockClientManager, isClientInside(testing::_))
+    .WillOnce(testing::Return(false))
+    .WillOnce(testing::Return(true));
+
+    EXPECT_CALL(*mockTableManager, finalizeDailyReport(testing::Eq(endTime)));
+
+    EXPECT_CALL(*mockClientManager, getAllClientNames())
+        .WillOnce(testing::Return(std::vector<std::string>{}));
+
+    EXPECT_CALL(*mockClientManager, registerClient(testing::_));
+
+    EXPECT_CALL(*mockClientManager, getClient(testing::_));
+
+    EXPECT_CALL(*mockClientManager, unregisterClient(testing::_, testing::_));
+
+    EXPECT_CALL(*mockTableManager, releaseTable(testing::_, testing::_));
+
+    EXPECT_CALL(*mockTableManager, getNextInQueue())
+        .WillOnce(testing::Return(""));
+
     EventManager eventManager(
         "test_file.txt",
         mockTableManager,
@@ -83,8 +78,28 @@ TEST (EventManagerTest, TestProcessEvents) {
         "3",
         "09:00 19:00",
         "10",
-        "08:41 1 client1",
+        "09:41 1 client1",
+        "09:51 4 client1",
     };
+
+    bool result = eventManager.loadEvents(eventLines);
+    eventManager.processEvents();
+    std::vector<EventManager::Event> eventLog = eventManager.getEventLog();
+
+    int eventType = static_cast<int>(eventLog.back().id);
+
+    EXPECT_EQ( eventType, 4 );
+}
+
+
+TEST (EventManagerTest, TestProcessEventsError) {
+        int tableCount = 3;
+    int hourlyRate = 10;
+    TimeUtil startTime(9, 0);
+    TimeUtil endTime(19, 0);
+
+    MockTableManager* mockTableManager = new MockTableManager(tableCount, hourlyRate);
+    MockClientManager* mockClientManager = new MockClientManager();
 
     EXPECT_CALL(*mockClientManager, isClientInside(testing::_))
     .WillOnce(testing::Return(false));
@@ -94,24 +109,6 @@ TEST (EventManagerTest, TestProcessEvents) {
     EXPECT_CALL(*mockClientManager, getAllClientNames())
         .WillOnce(testing::Return(std::vector<std::string>{}));
 
-    bool result = eventManager.loadEvents(eventLines);
-    eventManager.processEvents();
-    std::vector<EventManager::Event> eventLog = eventManager.getEventLog();
-
-    int eventType = static_cast<int>(eventLog.back().id);
-
-    EXPECT_EQ( eventType, 13 );
-}
-
-TEST (EventManagerTest, TestGetEventLog) {
-    int tableCount = 3;
-    int hourlyRate = 10;
-    TimeUtil startTime(9, 0);
-    TimeUtil endTime(19, 0);
-
-    MockTableManager* mockTableManager = new MockTableManager(tableCount, hourlyRate);
-    MockClientManager* mockClientManager = new MockClientManager();
-
     EventManager eventManager(
         "test_file.txt",
         mockTableManager,
@@ -129,7 +126,11 @@ TEST (EventManagerTest, TestGetEventLog) {
         "08:41 1 client1",
     };
 
-    EXPECT_CALL(*mockClientManager, isClientInside(testing::_))
-    .WillOnce(testing::Return(false));
-    
+    bool result = eventManager.loadEvents(eventLines);
+    eventManager.processEvents();
+    std::vector<EventManager::Event> eventLog = eventManager.getEventLog();
+
+    int eventType = static_cast<int>(eventLog.back().id);
+
+    EXPECT_EQ( eventType, 13 );
 }
